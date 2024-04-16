38176ae829435b0c79f42159d5d1da7ff65cce59
Hariom patil <Hariompatil47@gmail.com>
2024-04-16 19:44:57 +0530

    setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements(file_path:str)-> List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+    return requirements    
+
+setup(name= 'mlprojects',
+      version = "0.0.01",
+      author= 'Hariom patil',
+      author_email='hariompatil47@gmail.com',
+      packages= find_packages(),
+      install_requirements = get_requirements("requirements.txt")
+)
